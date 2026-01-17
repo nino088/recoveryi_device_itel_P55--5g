c79b98d2520d6905f715af09c5d47775afab2b25
nino <pilopilosimon1990@gmail.com>
2026-01-17 21:39:28 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/itel/P661N
+DEVICE_PATH := device/itel/P661n
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
