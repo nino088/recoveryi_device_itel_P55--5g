df6e0061a1d00ae0ead7a1a8887b5f278e72ee10
nino <pilopilosimon1990@gmail.com>
2026-01-17 23:43:23 +0100

    Update and rename omni_P661N.mk to twrp_P661n.mk

---
+++ twrp_P661n.mk
@@ -0,0 +1,35 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from P661n device
+$(call inherit-product, device/itel/P661n/device.mk)
+
+PRODUCT_DEVICE := P661n
+PRODUCT_NAME := twrp_P661n
+PRODUCT_BRAND := ITEL
+PRODUCT_MODEL := itel P661n
+PRODUCT_MANUFACTURER := itel
+
+PRODUCT_GMS_CLIENTID_BASE := android-tecno
+
+# Hide Reflash TWRP & FUSE passthrough
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.twrp.vendor_boot=true \
+    persist.sys.fuse.passthrough.enable=true
