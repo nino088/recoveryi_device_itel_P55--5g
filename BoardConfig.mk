91223b0da32e51e5d676fa9bb57496bfd8142c18
nino <pilopilosimon1990@gmail.com>
2026-01-25 07:03:40 +0100

    Refactor architecture and CPU settings in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,20 +8,19 @@
 DEVICE_PATH := device/itel/P661n
 
 # Architecture
-TARGET_ARCH                := arm64
-TARGET_ARCH_VARIANT        := armv8-a
-TARGET_CPU_ABI             := arm64-v8a
-TARGET_CPU_ABI2            := 
-TARGET_CPU_VARIANT         := generic
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 := 
+TARGET_CPU_VARIANT := generic
 TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
-TARGET_2ND_ARCH                := arm
-TARGET_2ND_ARCH_VARIANT        := armv7-a-neon
-TARGET_2ND_CPU_ABI             := armeabi-v7a
-TARGET_2ND_CPU_ABI2            := armeabi
-TARGET_2ND_CPU_VARIANT         := generic
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
 TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
-TARGET_USES_64_BIT_BINDER := true
 
 ENABLE_CPUSETS := true
 ENABLE_SCHEDBOOST := true
@@ -114,6 +113,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# Treble
+BOARD_VNDK_VERSION := current
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
